baf09f7fb41be4e02fb0229413ee43d609575f1a
ВашÐEGo87 <gokov87@vk.com>
2023-06-07 21:43:14 +0300

    v1

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,13 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("animalsCage")
 public class Timer {
 
-    private Long nanoTime = System.nanoTime();
+    private final Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
