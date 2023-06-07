27c8f35e181a11e873f5779d2842f9e6a5dff826
ВашÐEGo87 <gokov87@vk.com>
2023-06-07 21:54:49 +0300

    v1.0

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 public class Dog extends Animal{
     @Override
     public String toString() {
-        return "Im a Angry Dog :)";
+        return "Im a Angry Dog:)";
     }
 }
